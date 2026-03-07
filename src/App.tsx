d324154a5e227c7d56dd27267c0dedd9d37034be
Estifanos Wendimu <teffsauce2@gmail.com>
2026-03-07 16:48:36 +0300

    added/modified components and dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export function App() {
   return (
     <main>
       <Hero />
+      <div className="z-0 min-h-screen"></div>
     </main>
   );
 }
---
+++ src/components/custom/Button.tsx
@@ -0,0 +1,28 @@
+import type { VariantProps } from 'class-variance-authority';
+import type { ComponentProps } from 'react';
+import { Button as BaseButton, buttonVariants } from '../ui/button';
+import { cn } from '@/lib/utils';
+import type { LucideIcon } from 'lucide-react';
+
+export default function Button({
+  children,
+  leftIcon: LeftIcon,
+  rightIcon: RightIcon,
+  className,
+  ...props
+}: ComponentProps<typeof BaseButton> &
+  VariantProps<typeof buttonVariants> & { leftIcon?: LucideIcon; rightIcon?: LucideIcon }) {
+  return (
+    <BaseButton
+      {...props}
+      className={cn(
+        className,
+        'group font-general relative z-10 inline-flex w-fit overflow-hidden rounded-full bg-yellow-200 px-7 py-4 text-xs text-black uppercase',
+      )}
+    >
+      {LeftIcon && <LeftIcon />}
+      {children}
+      {RightIcon && <RightIcon />}
+    </BaseButton>
+  );
+}
